6735a10b84fa6039cfabb0428ba0c2b80cb5a075
bbungsang94 <bbubgsang94@ajou.ac.kr>
2022-02-18 15:28:09 +0900

    detection 함수 인자 수정

--- Detectors/EfficientDetector.py
+++ Detectors/EfficientDetector.py
@@ -27,13 +27,11 @@ class EfficientDetector(AbstractDetector):
         else:# 로컬 모델
             self.Detector = None
 
-    def detection(self, image, display=False, save=False):
+    def detection(self, image):
         """Determines the locations of the vehicle in the image
 
                 Args:
                     image: image(tensor)
-                    display: show figure option
-                    save: on display, furthermore you want to save image
                 Returns:
                     list of bounding boxes: coordinates [y_up, x_left, y_down, x_right]
 
