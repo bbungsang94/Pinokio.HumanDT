54349a9bc6592b4ef501aa51f04929dba8b73d23
jungmingu97 <huyi35@ajou.ac.kr>
2022-02-28 13:56:25 +0900

    new video matrix 수정

--- utilities/projection_helper.py
+++ utilities/projection_helper.py
@@ -71,29 +71,29 @@ def hex_to_rgb(h):
 def get_matrix(xPt, yPt, video_name: str, matrix_list):
     matrices = matrix_list[video_name]
     if video_name == "LOADING DOCK F3 Rampa 13 - 14":
-        xPt -= 1280 * 0  # First Video
-        if ((-8 / 3 * xPt + 1600) >= yPt) and ((-25 / 39 * xPt + 1120) > yPt):
+        xPt -= 1592 * 0  # First Video
+        if ((-988 / 400 * xPt + 1852.5) >= yPt) and ((-123 / 157 * xPt + 1497) > yPt):
             matrix = matrices[0]
-        elif ((-8 / 3 * xPt + 1600) < yPt) and ((-25 / 39 * xPt + 1120) >= yPt):
+        elif ((-988 / 400 * xPt + 1852.5) < yPt) and ((-123 / 157 * xPt + 1497) >= yPt):
             matrix = matrices[1]
-        elif ((-8 / 3 * xPt + 1600) < yPt) and ((-25 / 39 * xPt + 1120) < yPt):
+        elif ((-988 / 400 * xPt + 1852.5) < yPt) and ((-123 / 157 * xPt + 1497) < yPt):
             matrix = matrices[2]
         return matrix
     elif video_name == "LOADING DOCK F3 Rampa 11-12":
-        xPt -= 1280 * 1 # Second Video
-        if ((-8 * xPt + 4800) >= yPt) and ((-35 / 29 * xPt + 1645) > yPt):
+        xPt -= 1592 * 1 # Second Video
+        if ((-24.7 * xPt + 16450) >= yPt) and ((-222 / 163 * xPt + 2268) > yPt):
             matrix = matrices[0]
-        elif ((-8 * xPt + 4800) < yPt) and ((-35 / 29 * xPt + 1645) >= yPt):
+        elif ((-24.7 * xPt + 16450) < yPt) and ((-222 / 163 * xPt + 2268) >= yPt):
             matrix = matrices[1]
-        elif ((-8 * xPt + 4800) < yPt) and ((-35 / 29 * xPt + 1645) < yPt):
+        elif ((-24.7 * xPt + 16450) < yPt) and ((-222 / 163 * xPt + 2268) < yPt):
             matrix = matrices[2]
         return matrix
     elif video_name == "LOADING DOCK F3 Rampa 9-10":
-        xPt -= 1280 * 2  # Third Video
-        if ((2 * xPt - 100) <= yPt) and ((-80 / 9 * xPt + 6667) > yPt):
+        xPt -= 1592 * 2  # Third Video
+        if ((247 / 105 * xPt - 188) <= yPt) and ((-988 / 85 * xPt + 10461) > yPt):
             matrix = matrices[0]
-        elif ((2 * xPt - 100) > yPt) and ((-80 / 9 * xPt + 6667) >= yPt):
+        elif ((247 / 105 * xPt - 188) > yPt) and ((-988 / 85 * xPt + 10461) >= yPt):
             matrix = matrices[1]
-        elif ((2 * xPt - 100) > yPt) and ((-80 / 9 * xPt + 6667) < yPt):
+        elif ((247 / 105 * xPt - 188) > yPt) and ((-988 / 85 * xPt + 10461) < yPt):
             matrix = matrices[2]
         return matrix
