2ad5b9e34d72008c16f205664b3cb70a9ede42fa
jungmingu97 <huyi35@ajou.ac.kr>
2022-04-27 09:18:50 +0900

    123

--- GUI/HumanDT.UI/VideoForm.cs
+++ GUI/HumanDT.UI/VideoForm.cs
@@ -86,8 +86,8 @@ namespace HumanDT.UI
             directory = GetParent(6, directory);
             _MatrixPath = directory.FullName + "\\API\\params\\projection";
 
-            _Config.CondaEnv = "VDT";
-            //_Config.CondaEnv = "";
+            //_Config.CondaEnv = "VDT";
+            _Config.CondaEnv = "";
 
 
 
